1bba5ee4196ce3a61442d70de190a93d4935adbc
oTAETAEo <ggg7515@naver.com>
2025-03-19 18:08:30 +0900

    Initial commit

---
+++ src/test/java/com/jojoldu/book/spring_webservice/SpringWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.spring_webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
